fb50c9fd4e24085112aec792c868b9605096a000
Shikha Mahapatra <shikha.mahapatra4@gmail.com>
2022-02-22 12:16:45 +0530

    deleted unstructured java files

--- Java/Activities/Book.java
+++
@@ -1,9 +0,0 @@
-package activities;
-
-public abstract class Book {
-    String title;
-    public abstract void setTitle(String title) ;
-        public String getTitle(){
-           return title;
-    }
-}
