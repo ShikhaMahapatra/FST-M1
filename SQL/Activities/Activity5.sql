a857d280b5471cc4dd64387e8d5bc10eff1dc82c
Shikha Mahapatra <shikha.mahapatra4@gmail.com>
2022-02-26 13:27:18 +0530

    SQL Activities

---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,10 @@
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+-- Update the grade score of James Hoog to 300.
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+-- Update the name McLyon to Pierre.
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+-- Display modified data
+SELECT * FROM salesman;
